070a6e135298506bfd7b40aba60a9a211308eff7
Mby9 <jsmayankyadav@gmail.com>
2019-03-06 19:53:58 +0530

    Practise 06/03/2019 External modules

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,35 +3,3 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-class OldSyntax {
-    constructor() {
-        this.name = "Mayank";
-        // this.getGreeting = this.getGreeting.bind(this);
-        // this is a fix for "this" but not a very good one.    
-    }
-    getGreeting() {
-        return `hi. My name is ${this.name}`
-    }
-}
-
-const oldSyntax = new OldSyntax();
-console.log(oldSyntax);
-console.log(oldSyntax.getGreeting());
-const getGreeting = oldSyntax.getGreeting;
-//console.log(getGreeting());     // Here we get error becaue "this" is lost
-
-
-
-// -------------------- //
-
-class NewSyntax {
-    name = 'Jan';
-    getGreeting = () => {
-        return `hi. My name is ${this.name}`
-    }
-}
-const newSyntax = new NewSyntax();
-console.log(newSyntax);
-const newGetGreeting = newSyntax.getGreeting;
-console.log(newGetGreeting());
