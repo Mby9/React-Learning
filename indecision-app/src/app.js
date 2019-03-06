9e4310877066e3be76a530a20686f8fbeea97d03
Mby9 <jsmayankyadav@gmail.com>
2019-03-06 15:06:04 +0530

    Practise 06/03/2019 Breaking app into components

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,20 +1,5 @@
-// import "./utils.js";
-import subtactDefault, { square, add } from "./utils.js"; // the name should be same as exported
-import isSenior, { isAdult, canDrink } from "./person.js";
-import validator from 'validator';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-const template = <p>this is jsx</p>;
-ReactDOM.render(template, document.getElementById('app'));
-
-console.log("app.js is running.!!!");
-console.log(square(4));
-console.log(add(4, 10));
-console.log(subtactDefault(4, 10));
-
-console.log(isAdult(20));
-console.log(canDrink(100));
-console.log(isSenior(100));
-
-console.log(validator.isEmail('test@gmail.com')); 
+  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
